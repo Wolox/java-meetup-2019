b0622e436a2f9417e0047e73043690d962075b3f
Leandro Costa <leandro.costa@wolox.com.ar>
2019-07-15 16:40:57 -0300

    Add annotations to configure the test environment

--- src/test/java/ar.com.wolox.javameetup2019/JeringozaProcessorSpec.groovy
+++ src/test/java/ar.com.wolox.javameetup2019/JeringozaProcessorSpec.groovy
@@ -1,32 +1,41 @@
 package ar.com.wolox.javameetup2019
 
 import ar.com.wolox.javameetup2019.processor.JerigonzaProcessor
+import org.apache.camel.CamelContext
 import org.apache.camel.Exchange
 import org.apache.camel.builder.ExchangeBuilder
 import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.test.spring.CamelSpringBootRunner
 import org.apache.commons.lang3.RandomStringUtils
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.junit4.SpringRunner
 import spock.lang.Specification
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = [JerigonzaProcessor.class])
 @SpringBootTest
-class JeringozaProcessorSpec  extends  Specification {
+@RunWith(CamelSpringBootRunner.class)
+@EnableAutoConfiguration
+class JeringozaProcessorSpec extends Specification {
 
     @Autowired
     JerigonzaProcessor processor
 
+    @Autowired
+    CamelContext context
+
+    @Test
     def "Translate a random text to jeringozo"() {
         given: "generate a random text"
         def text = RandomStringUtils.randomAlphanumeric(100)
         and: "create a exchange"
-        def exchange = ExchangeBuilder.anExchange(new DefaultCamelContext())
+        def exchange = ExchangeBuilder.anExchange(context)
                 .withBody(text)
                 .build()
         when: "call jeringozo process"
