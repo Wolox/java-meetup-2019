8932caf8a26c504635b7b9821cc7ddebdc1dbb97
Leandro Costa <leandro.costa@wolox.com.ar>
2019-07-18 19:36:51 -0300

    Translate spanish to english + Change app port

--- src/main/java/ar/com/wolox/javameetup2019/helper/MessagesConstants.java
+++ src/main/java/ar/com/wolox/javameetup2019/helper/MessagesConstants.java
@@ -2,20 +2,20 @@ package ar.com.wolox.javameetup2019.helper;
 
 public class MessagesConstants {
 
-    public static final String EMPTY_BODY = "El cuer[po del mensaje está vacío. " +
-            "Por favor cargue los datos necesarios para dar de alta al cliente.";
-    public static final String EMPTY_NAME = "El campo name no fue ingresado.";
-    public static final String EMPTY_LASTNAME = "El campo last_name no fue ingresado.";
-    public static final String EMPTY_DOCUMENT_TYPE = "El campo document_type no fue ingresado.";
-    public static final String EMPTY_DOCUMENT_NUMBER = "El campo document_number no fue ingresado.";
-    public static final String EXISTENT_DNI =  "El documento ingresado ya existe.";
-    public static final String INCORRECT_DOCUMENT_TYPE = "El campo document_type es incorrecto. " +
-            "El tipo sólo puede ser dni o cuil";
-    public static final String INCORRECT_DNI = "El dni ingresado es inválido.";
-    public static final String INCORRECT_CUIL = "El cuil ingresado es inválido.";
-    public static final String INVALID_CUIL = "El cuil ingresado no es válido (se requieren 11 caracteres numéricos).";
-    public static final String NONEXISTENT_CLIENT = "No existe un cliente registrado con los datos proporcionados.";
-    public static final String SAVED_CLIENT = "El cliente fue guardado con éxito.";
+    public static final String EMPTY_BODY = "The message body is empty. " +
+            "Please, fill the necessary fields for creating the client.";
+    public static final String EMPTY_NAME = "The field name is empty.";
+    public static final String EMPTY_LASTNAME = "The field last_name is empty.";
+    public static final String EMPTY_DOCUMENT_TYPE = "The field document_type is empty.";
+    public static final String EMPTY_DOCUMENT_NUMBER = "The field document_number is empty.";
+    public static final String EXISTENT_DNI =  "A client with this document already exists.";
+    public static final String INCORRECT_DOCUMENT_TYPE = "The field document_type is wrong. " +
+            "It only can be dni or cuil.";
+    public static final String INCORRECT_DNI = "The dni is not valid.";
+    public static final String INCORRECT_CUIL = "The cuil is not valid.";
+    public static final String INVALID_CUIL = "The cuil is not valid (it has to be a number with 11 digits).";
+    public static final String NONEXISTENT_CLIENT = "There is no client found with the given information.";
+    public static final String SAVED_CLIENT = "The client was correctly created.";
 
     public static final String RESPONSE_OK = "Successful";
     public static final String RESPONSE_INTERNAL_ERROR = "Internal Server error";
